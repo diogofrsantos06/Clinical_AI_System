55e4dd57913550173472493558c20694f414df05
diogofrsantos <diogosantos061103@gmail.com>
2026-04-14 17:02:53 +0100

    changes

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -33,7 +33,10 @@ SECRET_KEY = "django-insecure-#zcmua=qhhvzp)rzjjj_jv3g8k+w+et#-y*zuy8%vi)!6#l^-z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [h.strip() for h in os.getenv(
+    "DJANGO_ALLOWED_HOSTS",
+    "localhost,127.0.0.1"
+).split(",") if h.strip()]
 
 # Application definition
 
